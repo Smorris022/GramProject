8073264a0f472d97ffb0e920aee8f150bac2710b
scm993 <scm993@gmail.com>
2016-05-28 21:46:23 -0400

    Initial Commit

---
+++ GramProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GramProject
+//
+//  Created by stephan morris on 5/28/16.
+//  Copyright © 2016 morrs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
